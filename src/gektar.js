561c23f6418efee14a053861660cccc93f9f89bb
Kiriuha-lucky <Kiriuha777@gmail.com>
2021-10-12 22:49:24 +1200

    add

--- src/gektar.js
+++ src/gektar.js
@@ -1,105 +1,107 @@
 'use strict';
 
-"use strict";
-const excelToJson = require("convert-excel-to-json");
- 
- constresult = excelToJson({
-    sourceFile:'example.xls'
-});
+let str = document.querySelector('#vri');
+
+let vriButton = document.querySelector('.vri__button');
 
 
-// let str = document.querySelector('#vri');
+let stat = (str) => {
+    let replaceNumber = str.value.replace(/[0-9,.]/g, '');
 
-// let vriButton = document.querySelector('.vri__button');
+    let arr = replaceNumber.split(/\n|;/).map((a) => a.toLowerCase()).map((a) => a.trim()).filter(element => element !== "");
 
+    let obj = {};
 
-// let stat = (str) => {
-//     let replaceNumber = str.value.replace(/[0-9,.]/g, '');
+    let toObj = (str) => {
+        if (obj.hasOwnProperty(str)) {
+            return obj[str] += 1;
+        }
+        return obj[str] = 1;
+    };
 
-//     let arr = replaceNumber.split(/\n|;/).map((a) => a.toLowerCase()).map((a) => a.trim()).filter(element => element !== "");
+    arr.map((a) => { return toObj(a) });
 
-//     let obj = {};
+    let objectArray = Object.entries(obj);
 
-//     let toObj = (str) => {
-//         if (obj.hasOwnProperty(str)) {
-//             return obj[str] += 1;
-//         }
-//         return obj[str] = 1;
-//     };
+    let ar = [];
 
-//     arr.map((a) => { return toObj(a) });
+    console.log(ar);
 
-//     let objectArray = Object.entries(obj);
+    objectArray.forEach(([key, value]) => {
+        ar.push({ key, value: +value })
+    })
 
-//     let ar = [];
+    return ar.sort((a, b) => b.value - a.value);
 
-//     objectArray.forEach(([key, value]) => {
-//         ar.push({ key, value: +value })
-//     })
+    // console.log(arr);
+}
 
-//     return ar.sort((a, b) => b.value - a.value);
-
-//     console.log(ar);
-// }
-
-// let root = document.querySelector('.root');
-
-// let render = (arr) => {
-//     let fragment = document.createDocumentFragment();
-
-//     let ul = document.createElement('ul');
-//     ul.className = "list-group";
-
-//     arr.forEach(({ key, value }) => {
-//         console.log(key);
-//         let li = document.createElement('li');
-//         li.innerHTML = `${key}: ${value}`;
-//         li.className = "list-group-item";
-//         return ul.appendChild(li)
-//     });
+let root = document.querySelector('.root');
 
 
-//     fragment.appendChild(ul);
 
 
-//     root.appendChild(fragment);
-// };
+let statList = {
+    izhs: {
+        name: 'для индивидуального жилищного строительства',
+        count: 0
+    },
 
-// vriButton.addEventListener('click', () => str ? render(stat(str)) : null);
+}
+
+let statListFilter = {
+    izhs: [
+        'для индивидуального жилищного строительства',
+        'для ведения личного подсобного хозяйства (приусадебный земельный участок)'
+    ]
+};
+
+let sumVri = (stat, arr) => {
+    let arrVri = {};
+
+    let keys = Object.keys(stat);
+    console.log(keys);
+
+    let obj = arr.map((a)=> a[key]);
+    // .reduce((acc, el) => ({ ...acc, ...el }), {});
 
 
-// // var xhr = new XMLHttpRequest()
-// // xhr.open('GET', 'https://6.react.pages.academy/wtw/films')
-// // xhr.send()
-// // xhr.onreadystatechange = function () { // (3)
-// //     if (xhr.readyState != 4) return;
+    console.log(obj);
 
-// //     console.log(true);
-// //     if (xhr.status != 200) {
-// //         alert(xhr.status + ': ' + xhr.statusText);
-// //     } else {
-// //         alert(xhr.responseText);
-// //     }
+    let sum = (ar) => ar.reduce((acc, cur) => acc + obj[cur], 0);
 
-// // }
+    keys.map((key) => arrVri[key] = sum(stat[key]));
 
-// console.log('Hello');
+    console.log(arrVri);
 
-// index.js
-// создание свойства класса без конструктора
-// class Game {
-//     name = 'Violin Charades'
-// }
-// const myGame = new Game()
 
-// создаем параграф
-// const p = document.createElement('p')
-// p.textContent = `I like ${myGame.name}.`
+};
 
-// создаем элемент заголовка
-// const heading = document.createElement('h1')
-// heading.textContent = 'Как интересно!'
 
-// добавляем параграф и заголовок в DOM
-// const root = document.querySelector('#root')
-// root.append(heading, p)
+let render = (arr) => {
+    console.log(arr);
+    sumVri(statListFilter, stat(str));
+
+    let fragment = document.createDocumentFragment();
+
+    let ul = document.createElement('ul');
+    ul.className = "list-group";
+
+    arr.forEach(({ key, value }) => {
+        // console.log(key);
+        let li = document.createElement('li');
+        li.innerHTML = `${key}: ${value}`;
+        li.className = "list-group-item";
+        return ul.appendChild(li)
+    });
+
+
+    fragment.appendChild(ul);
+
+
+    root.appendChild(fragment);
+};
+
+vriButton.addEventListener('click', () => str ? render(stat(str)) : null);
+
+
